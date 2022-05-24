1135cd93944e9cf56ce9f015fcac736323d3abc6
diegogithub-coding <54487369+diegogithub-coding@users.noreply.github.com>
2022-05-23 18:19:50 -0600

    Add files via upload

---
+++ end.js
@@ -0,0 +1,38 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+
+finalScore.innerText = mostRecentScore;
+username.addEventListener("keyup",() => {
+    //console.log(username.value);
+    saveScoreBtn.disabled = !username.value;
+});
+saveHighScore = e => {
+    console.log("Clicked the save button");
+    e.preventDefault();
+
+    const score = {
+        score : mostRecentScore,
+        name : username.value,
+        dateTime:  new Date(new Date().getTime()),//Math.floor((new Date()).getTime() / 1000) //+ new Date()
+        exerciseName: "First Test"
+    };
+    const jsonObjectToDataBase = JSON.stringify(score);
+    console.log(jsonObjectToDataBase);
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST","https://script.google.com/macros/s/AKfycbyPCXPLHkePS09gIqkHBwpY3KAbISRIVqAMxVHn_ihbCOmld4W5Ws-_kAcUkKjD-dLHoA/exec");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(jsonObjectToDataBase);
+
+     
+    // EN ESTA PAGINA HAY QUE CONECTAR G-SHEETS PARA PODER TENER LA DATA
+    //window.location.assign("quiz.html"); 
+
+    // investigar como ponerun mensjae que se envio el score. 
+
+};
+
