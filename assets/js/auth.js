9b8d1cb32e9cfbf4071ef598cb753a3bd299f27d
diegogithub-coding <54487369+diegogithub-coding@users.noreply.github.com>
2022-11-24 03:17:11 -0600

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -7,7 +7,7 @@ class Auth{
     
     validateAuth(auth){
         if(auth != 1){
-            window.location.replace("https://diegogithub-coding.github.io/accurateacademy/loginpage.html");
+            window.location.replace("/loginpage.html");
 
         }else{
             document.querySelector("body").style.display = "block";  // volver a mostrar la pagina cuano ya esta autenticado el user.
